d55cc7fb08810765c95aef6be2e11c1d92c5d816
Hashir Islam Khan <73400831+hashirislamkhan56@users.noreply.github.com>
2024-06-26 20:46:11 -0700

    Add files via upload

---
+++ Variables-1.py
@@ -0,0 +1,19 @@
+price = 10  #intg
+rating = 4.9  #float
+name = 'Hashir' #Strings
+is_published = False #Boolean
+print(price)
+
+#We check in a patient named Hashir Islam.He's 20 Years old and is a new patient.
+full_name = 'Hashir Islam'
+age = 20
+is_new = True
+
+#Receving Inputs
+name = input('What is your name? ')
+print('My name is ' + name)
+
+
+#Ask two questions: persons's name and favourite color. Then, print a message like "Mosh likes Blue"
+name = input('What is your name? ')
+colour = input('What is your favorite colour? ')
