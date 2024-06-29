f8edf16c567a8908b1545da1e4ecca4785114441
Hashir Islam Khan <73400831+hashirislamkhan56@users.noreply.github.com>
2024-06-29 12:25:32 -0700

    Add files via upload

---
+++ Arithmetic Operations-4.py
@@ -0,0 +1,37 @@
+print(10 / 3) #divison
+print(10 // 3 ) #integerdivision
+print(10 % 3 ) #modulo
+print(10 ** 3 ) #exponent
+
+x = 10
+x = x +3
+x +=3
+print(x)
+
+x = 10
+x -=3
+print(x)
+
+#Operator Precedence
+x_op = 10 + 3 * 2
+print(x_op)
+
+x_ops = 10 + 3 * 2 ** 2
+print(x_ops)
+
+x_opd = (10 + 3) * 2 ** 2
+print(x_opd)
+
+x_opf = (2+3) * 10 - 3
+print(x_opf)
+
+#Math Functions
+
+x_math = 2.9
+print(round(x_math)) #round figure value
+print(abs(-2.9)) #returns the positive value always
+
+import math
+print(math.ceil(4.9)) #ceiling of the number
+print(math.floor(4.9))
+
